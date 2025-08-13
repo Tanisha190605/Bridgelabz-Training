ab0eeece1e335e9563dde5d4a07a085d37878c39
Tanisha Gupta <tanishag1906@gmail.com>
2025-08-13 10:55:27 +0530

    Added project files

---
+++ KilometerToMile.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class KilometerToMile {
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    double kilometers=sc.nextDouble();
+    double miles=kilometers*0.621371;
+    System.out.println(miles);
+}
+    
+}
---
+++ VolumeOfCylinder.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class VolumeOfCylinder {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double radius=sc.nextInt();
+        double height=sc.nextInt();
+        double volume=Math.PI *radius*radius*height;
+        System.out.println(volume);
+    }
+}
