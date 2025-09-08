b1f7380d9b722fd0ff94654fdd002bf10e61c0f4
Tanisha Gupta <167095690+Tanisha190605@users.noreply.github.com>
2025-09-08 15:04:36 +0530

    Delete Temp.java

--- Temp.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Temp {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        float Celsius=sc.nextFloat();
-        float Fahrenheit = (Celsius * 9/5) + 32;
-        System.out.println(Fahrenheit);
-    }
-}
