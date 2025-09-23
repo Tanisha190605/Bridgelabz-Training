7c5783ff781da60a0a95411b8aa3ff71cbf1788a
Tanisha Gupta <167095690+Tanisha190605@users.noreply.github.com>
2025-09-23 08:44:54 +0530

    Update Animal_Hierarchy.java

--- Inheritence/Animal_Hierarchy.java
+++ Inheritence/Animal_Hierarchy.java
@@ -1,23 +1,16 @@
 package Inheritance;
-
-//Superclass
 class Animal {
  String name;
  int age;
-
- // Constructor
  public Animal(String name, int age) {
      this.name = name;
      this.age = age;
  }
 
- // Method to be overridden
  public void makeSound() {
      System.out.println("Some generic animal sound");
  }
 }
-
-//Subclass Dog
 class Dog extends Animal {
  public Dog(String name, int age) {
      super(name, age);
@@ -28,8 +21,6 @@ class Dog extends Animal {
      System.out.println(name + " the Dog says: Woof Woof!");
  }
 }
-
-//Subclass Cat
 class Cat extends Animal {
  public Cat(String name, int age) {
      super(name, age);
@@ -40,8 +31,6 @@ class Cat extends Animal {
      System.out.println(name + " the Cat says: Meow!");
  }
 }
-
-//Subclass Bird
 class Bird extends Animal {
  public Bird(String name, int age) {
      super(name, age);
@@ -58,12 +47,12 @@ public class Animal_Hierarchy {
      Animal dog = new Dog("Buddy", 3);
      Animal cat = new Cat("Whiskers", 2);
      Animal bird = new Bird("Tweety", 1);
-
-     // Polymorphism: same method behaves differently
+  
      dog.makeSound();
      cat.makeSound();
      bird.makeSound();
  }
 }
+
 
 
